c2889bd3bcfc2818104ae0f1a68be9c2df8d46cd
mcareggio <marcoscareggio@gmail.com>
2022-07-27 19:55:11 -0300

    Panel de bienvenida

--- src/app/components/welcomeart/welcomeart.component.ts
+++ src/app/components/welcomeart/welcomeart.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+const SALUDO="Hi there, I'm";
+const NOMBRE="Marcos Careggio";
+const DEVCLASS="Full-Stack Developer";
+const AVATARSRC="assets/img/avatar.png";
+const BUTTONTEXT="See More";
 @Component({
   selector: 'app-welcomeart',
   templateUrl: './welcomeart.component.html',
   styleUrls: ['./welcomeart.component.css']
 })
 export class WelcomeartComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public saludo;
+  public nombre;
+  public devclasificacion;
+  public avatarsrc;
+  public buttontext;
+  constructor() { 
+    this.saludo=SALUDO;
+    this.nombre=NOMBRE;
+    this.devclasificacion=DEVCLASS;
+    this.avatarsrc=AVATARSRC;
+    this.buttontext=BUTTONTEXT;
   }
 
+  ngOnInit(): void {
+
+  }
+  btnSeeMore(){}
 }
