ea293569b02760af4544978250debe0367c5c407
mcareggio <marcoscareggio@gmail.com>
2022-07-28 20:32:20 -0300

    Skils Terminado

--- src/app/components/skils/skils.component.ts
+++ src/app/components/skils/skils.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 const SKILS=[
-  { iconPath:'',
+  { iconPath:'assets/icons/mayormenor.svg',
     title:'Web Full Stack',
     text:'FrontEnd and BackEnd Dev.Frontend Html, Css, Angular, Typescript.'
   },
-  { iconPath:'',
+  { iconPath:'assets/icons/androidlogo.svg',
     title:'Android Java',
     text:'A Fullstack Android App developer in Java Language.'
   },
-  { iconPath:'',
+  { iconPath:'assets/icons/javaicon.svg',
     title:'Java Desktop',
     text:'A Fullstack Java Descktop in Java Language.'
   }
