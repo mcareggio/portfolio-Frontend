fa281c5e1bdb00ac2076842aab7c0d54ed4eea8a
Marcos Careggio <marcoscareggio@gmail.com>
2022-09-02 17:35:22 -0300

    Formulario de Login Creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path:'',component:LandingpageComponent},
+  {path:'login',component:LoginComponent}
 ];
 
 @NgModule({
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public formLogin:FormGroup;
 
-  constructor() { }
+  constructor(private formbuilder:FormBuilder) {
+    this.formLogin=this.formbuilder.group(  
+      {nombredeusuario:['',[Validators.required,Validators.maxLength(30)]],
+      password:['',[Validators.required,Validators.maxLength(30)]]
+      }
+    );
+   }
 
   ngOnInit(): void {
   }
+  get Password(){
+    return this.formLogin.get("password");
+  }
+  get Nombredeusuario(){
+    return this.formLogin.get("nombredeusuario");
+  } 
+  public onSubmit(evnt:Event){
+    evnt.preventDefault;
+    if(this.formLogin.valid){
 
+    }
+    else{
+      this.formLogin.markAllAsTouched();
+    }
+  }
 }
